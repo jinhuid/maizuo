570bbf5dbdbae1b6e00b840ae473c3aa272a813d
Huid <1732122592@qq.com>
2024-01-23 20:32:00 +0800

    Add dome 添加界面预览图片

--- 1.js
+++
@@ -1,8 +0,0 @@
-const arr = [1, 2, 3, 4, 5, 6]
-
-const sum = (arr, i = 0) => i === arr.length ? 0 : arr[i] + sum(arr, i + 1)
-console.log(sum(arr, 0));
-
-const factorial = (n) => n === 1 ? 1 : n * factorial(n - 1)
-
-console.log(factorial(5));
