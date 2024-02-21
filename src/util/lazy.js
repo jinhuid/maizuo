943e26722740d04c16a5af8c31b04d9f392c4033
Hui <1732122592@qq.com>
2024-02-22 07:28:36 +0800

    Change 修改配置文件

--- src/util/lazy.js
+++ src/util/lazy.js
@@ -2,10 +2,13 @@ import Vue from 'vue';
 export default {
     inserted(el, binding) {
         const observer = new IntersectionObserver(entries => {
-            if (entries[0].intersectionRatio > 0) {
+            if(entries[0].isIntersecting){
                 Vue.set(binding.value, "show", true);
                 observer.unobserve(el)
             }
+        },{
+            // 当目标元素与视口重叠至以下比例时 执行回调函数
+            threshold:[0]
         })
         observer.observe(el)
     }
